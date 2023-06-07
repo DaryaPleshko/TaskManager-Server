b24bb3f98a0b5c6fee515fdeb4bb7dad61d190fe
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-06-07 22:16:00 +0300

    commit

--- src/repository/api.repository.js
+++ src/repository/api.repository.js
@@ -1,23 +1,12 @@
 const { pool } = require('../db');
 
-const createAuthDB = async (email, pwd) => {
+const getUserByEmail = async (email) => {
     const client = await pool.connect();
-    try {
-        await client.query('BEGIN');
 
-        const sql = `
-        INSERT INTO users(email, pwd)
-        VALUES ($1, $2) RETURNING *`;
-        const gettingSql = (await client.query(sql, [email, pwd])).rows;
+    const sql = `SELECT * FROM users WHERE email = $1`;
+    const gettingSql = (await client.query(sql, [email])).rows;
 
-        await client.query('COMMIT');
-
-        return gettingSql;
-    } catch (error) {
-        await client.query('ROLLBACK');
-
-        throw new Error(error.message);
-    }
+    return gettingSql;
 }
 
 const createRegDB = async (name, surname, email, pwd) => {
@@ -40,4 +29,4 @@ const createRegDB = async (name, surname, email, pwd) => {
     }
 }
 
-module.exports = { createAuthDB, createRegDB }
+module.exports = { getUserByEmail, createRegDB }
--- src/service/api.service.js
+++ src/service/api.service.js
@@ -1,13 +1,16 @@
 const bcrypt = require('bcrypt');
-const { createAuthDB, createRegDB } = require('../repository/api.repository');
+const { getUserByEmail, createRegDB } = require('../repository/api.repository');
 
 
-const salt = 12;
+const salt = 2;
 
 const createAuth = async (email, pwd) => {
-    const data = await createAuthDB(email, pwd);
-    if (!data.length) throw new Error(`The value didn't come from the client`);
-    return data;
+    const user = await getUserByEmail(email);
+
+    if (!user.length) throw new Error(`email not found`);
+
+    if (!(await bcrypt.compare(pwd, user[0].pwd))) throw new Error(`Password don't match`);
+    return user;
 }
 
 const createReg = async (name, surname, email, pwd) => {
