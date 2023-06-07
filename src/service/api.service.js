8af1b1cbe6346ce1723ba4fa7da918cdd95d6cc5
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-06-07 21:28:05 +0300

    commit

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -1,4 +1,8 @@
+const bcrypt = require('bcrypt');
 const { createAuthDB, createRegDB } = require('../repository/api.repository');
+
+
+const salt = 12;
 
 const createAuth = async (email, pwd) => {
     const data = await createAuthDB(email, pwd);
@@ -7,7 +11,9 @@ const createAuth = async (email, pwd) => {
 }
 
 const createReg = async (name, surname, email, pwd) => {
-    const data = await createRegDB(name, surname, email, pwd);
+    const hashPwd = await bcrypt.hash(pwd, salt);
+
+    const data = await createRegDB(name, surname, email, hashPwd);
     if (!data.length) throw new Error(`The value didn't come from the client`);
     return data;
 }
